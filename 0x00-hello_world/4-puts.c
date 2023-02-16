d62c19e5de5bf7935409fc9bc4c9063b2feaba7f
Uche Ubaka <stephen.ubaka@gmail.com>
2023-02-16 01:44:08 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main-a program that prints a line with print function
+ * main - a program that prints a line with print function
  * Return 0(success)
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
