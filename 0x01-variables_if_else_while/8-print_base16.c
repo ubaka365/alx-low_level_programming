09eba2873a895d46a83d20d15ce80ba608b108a7
Uche Ubaka <stephen.ubaka@gmail.com>
2023-02-19 00:51:25 -0800

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - programs that prints all base 16 in lowercase
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+	char la;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar((num % 10) + '0');
+	}
+
+	for (la = 'a'; la < 'f'; la++)
+	{
+		putchar(la);
+	}
+	putchar('\n');
+	return (0);
+}
