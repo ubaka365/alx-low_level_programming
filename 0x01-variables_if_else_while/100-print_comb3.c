cba95b62fbc0392465f1441a4ffca9d9b09d8a58
Uche Ubaka <stephen.ubaka@gmail.com>
2023-02-19 15:50:49 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - program to print all possible combinations of two digits
+ * Return: 0
+ */
+
+int main(void)
+{
+	int ones = '0';
+	int tens = '0';
+
+	for (tens = '0'; tens <= '9'; tens++)/*prints tens digit*/
+	{
+		for (ones = '0'; ones <= '9'; ones++)/*prints ones digit*/
+		{
+			if (!((ones == tens) || (tens > ones)))/*eliminates repition*/
+			{
+				putchar(tens);
+				putchar(ones);
+				if (!(ones == '9' && ones == '8'))/*adds comma and space*/
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
