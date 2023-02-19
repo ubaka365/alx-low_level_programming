3393c1e409c3550109b5f8bae6283dee37038cc4
Uche Ubaka <stephen.ubaka@gmail.com>
2023-02-18 21:05:53 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - function of numberss from 0-9
+ * Return: 0 (sucsess)
+ *
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d" num);
+	}
+	printf('\n');
+	return (0);
+}
