374194f5320014c2ecc19d53346586d82a4aa903
Uche Ubaka <stephen.ubaka@gmail.com>
2023-02-22 09:58:28 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower - checks if character is lowercase
+ * @c: the character
+ * Return:  1 if letter is lowercase, 0 if not
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+
+	return (1);
+	}
+	return (0);
+}
