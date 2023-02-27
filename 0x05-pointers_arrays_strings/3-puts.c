40f8b9d942f730cae9d40c84e861c786c00bef73
Uche Ubaka <stephen.ubaka@gmail.com>
2023-02-27 12:15:35 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - prints a string, followed by a new line
+ * @str : string
+ */
+
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
