3f75a4a0641df005ca28a4ba04228e1071c2cda5
Uche Ubaka <stephen.ubaka@gmail.com>
2023-02-27 05:39:58 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print the numbers since 0 up to 9
+ * Return: The number since 0 is up to 9
+ */
+
+void print_numbers(void)
+{
+int a;
+
+for (a = 48; a <= 58; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
