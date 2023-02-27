ca0f3157c44adca104235f0b00661cfd74985132
Uche Ubaka <stephen.ubaka@gmail.com>
2023-02-27 03:00:47 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @x: the first number to multiply
+ * @y: the second number to multiply
+ * Return: the value of the multiplication of x and y
+ */
+
+int mul(int x, int y)
+{
+	return (x * y);
+}
