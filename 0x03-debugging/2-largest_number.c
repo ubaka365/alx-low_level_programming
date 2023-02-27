9019b1fef74cbc090d03b5184788abcd9894617e
Uche Ubaka <stephen.ubaka@gmail.com>
2023-02-27 09:28:42 -0800

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,24 +9,17 @@
 
 int largest_number(int a, int b, int c)
 {
-int largest;
-
-if (a > b && a > c)
-{
-largest = a;
+	if (a >= b && a >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		largetst = b;
+	}
+	else
+	{
+		largest = c;
+	}
+	return (largest);
 }
-else if (a > b && c > a)
-{
-largest = c;
-}
-else if (b > c)
-{
-largest = b;
-}
-else
-{
-largest = c;
-}
-
-return (largest);
-}}
