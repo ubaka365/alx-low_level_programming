6eb74506d1707ded774c403494c368cdb54a64c4
Uche Ubaka <stephen.ubaka@gmail.com>
2023-02-27 01:30:16 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that returns length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	size_t length = 0;
+
+	while (*str++)
+	{
+		length++;
+	}
+	return (length);
+}
