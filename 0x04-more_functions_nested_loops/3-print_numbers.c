9f48b69077cfe15e9e830f14d223d93b1d7a9c22
Uche Ubaka <stephen.ubaka@gmail.com>
2023-03-02 00:16:11 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * print_numbers - print the numbers since 0 up to 9
- * Return: The number since 0 is up to 9
+ * print_numbers - checks for checks for a digit (0 through 9).
+ *
+ * Return: Always 0.
  */
-
 void print_numbers(void)
 {
-	int a;
+int c;
 
-	for (a = 48; a <= 58; a++)
-	{
-		_putchar(a + '0');
-	}
-	_putchar('\n');
+for (c = 48; c < 58; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
 }
