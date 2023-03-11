79b49e2d0c214077ab99b54b3b7b79282ef8c15c
Uche Ubaka <stephen.ubaka@gmail.com>
2023-03-10 22:44:34 -0800

    3-puts.c refixed

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,10 +1,13 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-
-void _puts(char *s)
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to print
+ */
+void _puts(char *str)
 {
-	return _puts(s);
+while (*str != '\0')
+{
+_putchar(*str++);
+}
+_putchar('\n');
 }
