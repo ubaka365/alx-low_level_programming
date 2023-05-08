4af10c65cf3f5fb1cd8601dc9d198b21ff3275c4
ubaka365 <stephen.ubaka@gmail.com>
2023-05-08 05:06:13 -0700

    0-read_textfile.c Refixed

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,42 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
-* read_textfile - reads a text file and prints its contents
-* @filename: a pointer to the name of the file to read
-* @letters: the maximum number of characters to read and print
-* This function reads a text file named @filename and prints its contents
-* to the standard output. It will read up to @letters characters or until
-* the end of the file is reached, whichever comes first.
-* Return: the actual number of characters read and printed
-*/
-
+ * read_textfile- Reads text, file prints to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t file, s, d;
-	char *text;
+	char *b;
+	ssize_t d;
+	ssize_t w;
+	ssize_t t;
 
-	text = malloc(letters);
-	if (text == NULL)
+	d = open(filename, O_RDONLY);
+	if (d == -1)
 		return (0);
+	b = malloc(sizeof(char) * letters);
+	t = read(d, b, letters);
+	w = write(STDOUT_FILENO, b, t);
 
-	if (filename == NULL)
-		return (0);
-
-	file = open(filename, O_RDONLY);
-
-	if (file == -1)
-	{
-		free(text);
-		return (0);
-	}
-
-	s  = read(file, text, letters);
-
-	d = write(STDOUT_FILENO, text, s);
-
-	close(file);
-
-	return (d);
+	free(b);
+	close(d);
+	return (w);
 }
